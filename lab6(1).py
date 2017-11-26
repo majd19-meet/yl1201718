2fe9ece3a5405d7c3cfc097c8e9e182c3a3a5e1a
majd <majd19-meet>
2017-11-26 19:20:47 +0200

    Lab6

--- lab6(1).py
+++ lab6(1).py
@@ -1,17 +1,41 @@
 from turtle import *
-colormode(225)
-class Square (Turtle):
-	def __init__(self,size):
+import random
+colormode(255)
+r = random.randint (0,255)
+g = random.randint (0,255)
+b = random.randint (0,255)
+#class Square(Turtle):
+#	def __init__(self,size):
+#		Turtle.__init__(self)
+#		self.shapesize(size)
+#		self.shape("square")
+
+#	def random_color(self):
+#		self.color(r,g,b)
+
+#square1 = Square(10)
+#square1.random_color()
+random.randint(0,225)
+#import turtle
+
+class Rectangle(Turtle):
+	def __init__(self,width,height):
 		Turtle.__init__(self)
-		self.shapesize(size)
-		self.shape("square")
+		self.width = 90
+		self.height = 50
 
-	def random_color(self):
-		r = random.randit (0-255)
-		g = random.randit (0-255)
-		b = random.randit (0-255)
-		self.color(r,g,b)
+		register_shape("Rectangle",((self.width,0),(self.width,self.height),(0,self.height),(0,0)))
+		self.shape("Rectengle")
+		self.setheading(90)
 
-Square1 = Square(10)
+class Square(Rectengle):
+	def __init__(self,)
+
+
+getshapes()
 
 mainloop()
+
+#mainloop()
+
+
